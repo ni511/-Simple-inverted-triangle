a48bcf3798084bb2db156201ccdf32f4e73d14b7
ni511 <103868519+ni511@users.noreply.github.com>
2022-05-31 00:38:14 +0530

    Update pattern2.cpp

--- pattern2.cpp
+++ pattern2.cpp
@@ -15,3 +15,21 @@ int main()
         cout<<endl;
     }
 }
+//And also this pattern will solved by
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j;
+    cout<<"Enter the no. of layers to be formed:\n";
+    cin>>n;
+    for(i=5;i>=1;i--)
+    {
+        for(j=i;j>=1;j--)
+        {
+            cout<<"*  ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
