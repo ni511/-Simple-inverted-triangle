6838ce9ea9fe8a29e60c98c8789277c702e02e4d
ni511 <103868519+ni511@users.noreply.github.com>
2022-05-31 00:25:01 +0530

    Add files via upload

---
+++ pattern2.cpp
@@ -0,0 +1,17 @@
+//Simple inverted triangle
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j;
+    cout<<"Enter the no. of layers of triangles needed:\n";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        for(j=5;j>=i;j--)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
